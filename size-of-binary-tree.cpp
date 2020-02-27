29b237b005a85045e2ca55dbe41ecbfee3069e6b
monu <mozis.bot@gmail.com>
2020-02-27 11:55:18 +0530

    Atom

--- size-of-binary-tree.cpp
+++ size-of-binary-tree.cpp
@@ -18,3 +18,17 @@ int getSize(Node* node)
    v.clear();
    return siz;
 }
+// second method
+/*
+int getSize(Node* node)
+{
+   // Your code here
+   if(node==NULL)
+   {
+       return 0;
+
+   }
+   return 1+getSize(node->left)+getSize(node->right);
+
+}
+*/
