8d719ff925b261cbd43745d606fc8380d6beec47
Monu Kumar <47026116+monu18@users.noreply.github.com>
2020-02-27 11:38:55 +0530

    Add files via upload

---
+++ size-of-binary-tree.cpp
@@ -0,0 +1,20 @@
+vector<int>v;
+void inorder(Node* node,vector<int>&v)
+{
+    if(node==NULL)
+    {
+        return ;
+    }
+    inorder(node->left,v);
+    v.push_back(node->data);
+    inorder(node->right,v);
+
+}
+int getSize(Node* node)
+{
+   // Your code here
+   inorder(node,v);
+   int siz=v.size();
+   v.clear();
+   return siz;
+}
